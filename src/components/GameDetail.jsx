2c44b72aaa2e325ad721026effe1ff8592ddd22f
Antonov-S <anto1nov1@gmail.com>
2023-12-27 20:28:18 +0200

    Adding isLoading flag to prevent rendering befor data fetching

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -4,37 +4,42 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export default function GameDetail() {
-    const { screen, game } = useSelector((state) => state.detail)
+    const { screen, game, isLoading } = useSelector((state) => state.detail);
+
     return (
-        <CardShadow>
-            <Detail>
-                <div className="stats">
-                    <div className="rating">
-                        <h3>{game.name}</h3>
-                        <p>Rating: {game.rating}</p>
-                    </div>
-                    <div className="info">
-                        <h3>Platforms</h3>
-                        <div className="platforms">
-                            {game.platforms.map((data) => (
-                                <h3 key={data.platform.id}>{data.platform.name}</h3>
+        <>
+            {!isLoading && (
+                <CardShadow>
+                    <Detail>
+                        <div className="stats">
+                            <div className="rating">
+                                <h3>{game.name}</h3>
+                                <p>Rating: {game.rating}</p>
+                            </div>
+                            <div className="info">
+                                <h3>Platforms</h3>
+                                <div className="platforms">
+                                    {game.platforms.map((data) => (
+                                        <h3 key={data.platform.id}>{data.platform.name}</h3>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="media">
+                            <img src={game.background_image} alt={game.background_image} />
+                        </div>
+                        <div className="description">
+                            <p>{game.description_raw}</p>
+                        </div>
+                        <div className="gallery">
+                            {screen.results.map((screen) => (
+                                <img key={screen.id} src={screen.image} alt={screen.image} />
                             ))}
                         </div>
-                    </div>
-                </div>
-                <div className="media">
-                    <img src={game.background_image} alt={game.background_image} />
-                </div>
-                <div className="description">
-                    <p>{game.description_raw}</p>
-                </div>
-                <div className="gallery">
-                    {screen.results.map((screen) => (
-                        <img key={screen.id} src={screen.image} alt={screen.image} />
-                    ))}
-                </div>
-            </Detail>
-        </CardShadow>
+                    </Detail>
+                </CardShadow>
+            )}
+        </>
     )
 }
 
